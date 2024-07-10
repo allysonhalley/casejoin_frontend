9d95f19376090eea47140dbe25b02f5b197061e6
allysonhalley <allysonhalley@gmail.com>
2024-07-10 03:44:02 -0300

    base structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
+import { CategoryShowComponent } from './components/category-show/category-show.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductShowComponent } from './components/product-show/product-show.component';
+import { CategoriesListComponent } from './components/categories-list/categories-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddCategoryComponent,
+    CategoryShowComponent,
+    AddProductComponent,
+    ProductsListComponent,
+    ProductShowComponent,
+    CategoriesListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/category-show/category-show.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-show',
+  templateUrl: './category-show.component.html',
+  styleUrl: './category-show.component.css'
+})
+export class CategoryShowComponent {
+
+}
